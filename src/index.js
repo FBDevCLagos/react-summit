e9355a02d22fefff3fb3ee1b36ebb7a5da538f51
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-07-05 08:49:03 +0100

    Mock interface for react summit 2018

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.css';
-import App from './App';
+import Home from './components/home/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
