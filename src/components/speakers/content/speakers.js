d955e9684a4dd9dd55c75f53741365e4fe892f36
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-07-30 14:01:23 +0100

    build speakers list components with styling

---
+++ src/components/speakers/content/speakers.js
@@ -0,0 +1,29 @@
+export const speakersList = [
+  {
+    name: "Ibekwe Laster",
+    title: "Software Engineer",
+    company: "Hooli",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "http://cdn02.cdn.justjaredjr.com/wp-content/uploads/headlines/2017/05/despicable-me-3-clip-minions-stage.jpg",
+    githubUrl: "#",
+    twitterUrl: "#",
+  },
+  {
+    name: "Jane Kayiwa",
+    title: "Software Engineer",
+    company: "Harvana Systems",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "https://cdn2.stylecraze.com/wp-content/uploads/2013/06/african-women1.jpg",
+    githubUrl: "#",
+    twitterUrl: "#",
+  },
+  {
+    name: "Jude Kenneth",
+    title: "Software Engineer",
+    company: "CroosOver Ltd.",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "https://techcrunch.com/wp-content/uploads/2015/10/makinde-adeagbo_founder.jpg",
+    githubUrl: "#",
+    twitterUrl: "#",
+  }
+];
