d1cdcd82c500f9bf814118ac43359e374509041f
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-08-01 00:33:50 +0100

    add components and styling for sponsors panel

--- src/components/sponsors/Sponsors.js
+++ src/components/sponsors/Sponsors.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+import SponsorCard from './SponsorCard';
+import './Sponsors.css';
+import { sponsorsList } from "./content/sponsors";
+
 export default () => (
   <div id="sponsors">
     <h2 className="side-panel__header-title">
       Sponsors
     </h2>
-    <p className="side-panel__content">
-      Sponsors list will be here
-    </p>
+    <div className="side-panel__content sponsors">
+      {
+        sponsorsList.map(speaker =>
+          <SponsorCard
+            name={speaker.name}
+            introText={speaker.introText}
+            image={speaker.image}
+          />)
+      }
+    </div>
+    {/*<p className="side-panel__content">*/}
+      {/*<div className="sponsor__wrapper">*/}
+        {/*<div className="sponsor__logo"></div>*/}
+        {/*<div className="sponsor__details"></div>*/}
+      {/*</div>*/}
+    {/*</p>*/}
   </div>
 );
