580d5c1532c83af5ff3c6c4fb2d1cf52e4f47cc2
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-08-01 00:33:50 +0100

    add components and styling for sponsors panel

---
+++ src/components/sponsors/content/sponsors.js
@@ -0,0 +1,22 @@
+export const sponsorsList = [
+  {
+    name: "SpaceApps",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png"
+  },
+  {
+    name: "Tech Space",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png"
+  },
+  {
+    name: "National Climate",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png"
+  },
+  {
+    name: "PrepSafe.co",
+    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png"
+  },
+];
