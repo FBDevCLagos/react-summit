06e8f1b558b52f84cf38ec52eca7b4a51c21c1a9
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-08-03 14:03:16 +0100

    add actual sponsors data

--- src/components/sponsors/content/sponsors.js
+++ src/components/sponsors/content/sponsors.js
@@ -1,26 +1,20 @@
 export const sponsorsList = [
   {
-    name: "SpaceApps",
-    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png",
-    url: "http://yourcompany.com"
+    name: "Facebook",
+    introText: "Developer Circles from Facebook is a great way for developers to connect and collaborate. Each Circle is locally organized by members of the community.",
+    image: "https://www.facebook.com/images/fb_icon_325x325.png",
+    url: "https://www.facebook.com/"
   },
   {
-    name: "Tech Space",
-    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png",
-    url: "http://yourcompany.com"
+    name: "Terragon Group",
+    introText: "We creatively leverage technology, data and optimization techniques to deliver value to businesses with interest in Africa.",
+    image: "https://i1.wp.com/innovation-village.com/wp-content/uploads/2016/08/Terragon-Group-e1509456357983.jpg",
+    url: "https://terragongroup.com/"
   },
   {
-    name: "National Climate",
-    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png",
-    url: "http://yourcompany.com"
-  },
-  {
-    name: "PrepSafe.co",
-    introText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: "https://cdn.dribbble.com/users/261302/screenshots/1361907/logoplaceholder.png",
-    url: "http://yourcompany.com"
+    name: "TechMeetupsNG",
+    introText: "TechMeetupsNG has been setup to create a network of Tech communities around Nigeria.",
+    image: "https://i1.wp.com/techmeetupsng.com/wp-content/uploads/2018/07/cropped-TMN-Name-Logo-v3-small.png?fit=748%2C176",
+    url: "http://techmeetupsng.com/"
   },
 ];
