832cc0fcb733fc127413518cfdd14467d1eb141e
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-08-03 14:11:57 +0100

    include andela as sponsor

--- src/components/sponsors/content/sponsors.js
+++ src/components/sponsors/content/sponsors.js
@@ -6,6 +6,12 @@ export const sponsorsList = [
     url: "https://www.facebook.com/"
   },
   {
+    name: "Andela",
+    introText: "Andela helps companies quickly and confidently scale their software teams with world-class, full-time developers.",
+    image: "https://pbs.twimg.com/profile_images/647544968777756672/-Ms_S6bh_400x400.png",
+    url: "https://terragongroup.com/"
+  },
+  {
     name: "Terragon Group",
     introText: "We creatively leverage technology, data and optimization techniques to deliver value to businesses with interest in Africa.",
     image: "https://i1.wp.com/innovation-village.com/wp-content/uploads/2016/08/Terragon-Group-e1509456357983.jpg",
