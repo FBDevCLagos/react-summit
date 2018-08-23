f0d9e1803e7053508f660ee5898075d7bfd19326
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-08-24 00:47:10 +0100

    Update speakers section with actual data

--- src/components/speakers/content/speakers.js
+++ src/components/speakers/content/speakers.js
@@ -1,20 +1,56 @@
 export const speakersList = [
   {
-    name: "Awesome Speaker",
-    title: "Front-end Engineer",
-    company: "Company",
-    introText: "All round nice guy :)",
-    image: "http://financialspectrum.com.au/wp-content/uploads/Headshot-Placeholder.png",
-    githubUrl: "",
-    twitterUrl: "",
+    name: "Kent Dodds",
+    title: "Javascript Engineer",
+    company: "Paypal",
+    introText: "",
+    image: "https://media.licdn.com/dms/image/C4D03AQH3q0cQM27tJw/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=lJo5RBGSelHrt0mByBCMpXhyHbuWwzjdSjpY9bduDFw",
+    githubUrl: "https://github.com/kentcdodds",
+    twitterUrl: "https://twitter.com/kentcdodds"
   },
   {
-    name: "Awesome Speaker",
+    name: "Barbara Mbanefo",
     title: "Software Engineer",
-    company: "Company",
-    introText: "All round nice lady :)",
-    image: "http://www.ieeeaustsb.org/files/2017/05/placeholder-female-square.png",
+    company: "Facebook",
+    introText: "",
+    image: "https://media.licdn.com/dms/image/C4E03AQGN4AgEb6rWzg/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=uGl8gf6_ZS7EIbILWbyMiN4LgddEPbVaSHUTlutoggk",
     githubUrl: "",
-    twitterUrl: "",
+    twitterUrl: "https://twitter.com/barbarambanefo",
+  },
+  {
+    name: "Miguel Jimenez Esun",
+    title: "Software Engineer",
+    company: "Facebook",
+    introText: "",
+    image: "https://avatars1.githubusercontent.com/u/829269?s=460&v=4",
+    githubUrl: "https://github.com/mjesun",
+    twitterUrl: "https://twitter.com/mjesun"
+  },
+  {
+    name: "Christian Nwamba",
+    title: "Content Creator / Technical Reviewer",
+    company: "Pusher",
+    introText: "",
+    image: "https://pbs.twimg.com/profile_images/725313461765427204/2Od92RM7_400x400.jpg",
+    githubUrl: "https://github.com/christiannwamba",
+    twitterUrl: "https://twitter.com/codebeast"
+  },
+  {
+    name: "Celestine Omin",
+    title: "Senior Technical Consultant",
+    company: "Andela",
+    introText: "",
+    image: "https://media.licdn.com/dms/image/C5603AQEYAraUiZHh7g/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=G8H4vPd26dkP1qJhyQuFO3WCgNfiDxVObfD9V3Luh_8",
+    githubUrl: "https://github.com/cyberomin",
+    twitterUrl: "https://twitter.com/cyberomin"
+  },
+  {
+    name: "Chinedu Daniel",
+    title: "Software Developer",
+    company: "Andela",
+    introText: "",
+    image: "https://media.licdn.com/dms/image/C5603AQEzGhypAHriqA/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=uv5ruHGlrj3FTkYnJDjFb5zpDydyg4-x8IZvK55qQZ0",
+    githubUrl: "https://github.com/blueyedgeek",
+    twitterUrl: "https://twitter.com/austinswaggz"
   },
 ];
