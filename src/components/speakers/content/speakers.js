5365388673b941e1096901823dc725d7b9dfa63c
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-08-28 15:21:32 +0100

    Add bio information for speakers

--- src/components/speakers/content/speakers.js
+++ src/components/speakers/content/speakers.js
@@ -3,16 +3,71 @@ export const speakersList = [
     name: "Kent Dodds",
     title: "Javascript Engineer",
     company: "Paypal",
-    introText: "",
+    introText: `
+      <p>
+        Kent C. Dodds works at PayPal as a full stack JavaScript Engineer.
+        He represents PayPal on the TC39 (Ecma International, Technical
+        Committee 39. He's actively involved in the open source community as a
+        creator and contributor. His commits are visible with little effort in
+        PayPal’s
+        <a
+          href="https://github.com/paypal/downshift" target="_blank">
+          downshift
+        </a>,
+        <a
+          href="https://github.com/kentcdodds/cross-env" target="_blank">
+          cross-env
+        </a>,
+        <a
+          href="https://github.com/kentcdodds/babel-plugin-macros"
+          target="_blank">
+          babel-plugin-macros
+        </a>
+      </p>
+
+      <p>
+        Kent’s frequent contributions as an Egghead.io instructor,
+        Frontend Masters instructor, and Google Developer Expert are among the
+        “bunch of stuff” he does in the React and general JavaScript ecosystem.
+      </p>
+
+      <p>
+        Kent is happily married and claims being a father of four kids
+        (among other things) doesn’t give him the time to add a bit more
+        color to his
+        <a href="https://kentcdodds.com/" target="_blank">personal homepage</a>.
+      </p>`
+        ,
     image: "https://media.licdn.com/dms/image/C4D03AQH3q0cQM27tJw/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=lJo5RBGSelHrt0mByBCMpXhyHbuWwzjdSjpY9bduDFw",
     githubUrl: "https://github.com/kentcdodds",
-    twitterUrl: "https://twitter.com/kentcdodds"
+    twitterUrl: "https://twitter.com/kentcdodds",
   },
   {
     name: "Barbara Mbanefo",
     title: "Software Engineer",
     company: "Facebook",
-    introText: "",
+    introText: `
+      <p>
+        Barbara Mbanefo works with the Facebook Engineering team as a Mobile
+        Software Engineer in the Lightweight Interfaces (LWI) team in the Ads
+        Growth Org. She developers software for the Facebook iPhone app that
+        enables businesses to connect with the right audiences, and obtain
+        meaningful results Using Objective-C, PHP, GraphQL and a few other
+        internal tools.
+      </p>
+
+      <p>
+        Barbara’s over 15 years experience as a Senior Software Engineer
+        starts in 2003 with companies like Siemens VDO, Motorola and Orange
+        among companies in her prestigious CV.
+      </p>
+
+      <p>
+        In her career span, Barbara has bagged over 6 engineering awards,
+        picked up three international languages and revisited her love
+        for Nigerian jollof :)
+      </p>
+    `,
     image: "https://media.licdn.com/dms/image/C4E03AQGN4AgEb6rWzg/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=uGl8gf6_ZS7EIbILWbyMiN4LgddEPbVaSHUTlutoggk",
     githubUrl: "",
     twitterUrl: "https://twitter.com/barbarambanefo",
@@ -21,10 +76,30 @@ export const speakersList = [
     name: "Miguel Jimenez Esun",
     title: "Software Engineer",
     company: "Facebook",
-    introText: "",
+    introText: `
+      <p>
+        Miguel is a Senior Software Engineer at Facebook. He is currently
+        working with the JS Foundation team - the team is in charge of Jest,
+        Yarn and Metro (React Native’s bundler), and developing infrastructure
+        to support all React Native and Web developers.
+      </p>
+
+      <p>
+        Before Miguel was at New Relic for over a year, and before that, at
+        Spotify for 3 years. With these teams, he has consistently built
+        product that optimizes for performance, speed (60 fps), ease of usage
+        (self-resizing, self-fetching charts) and memory utilization.
+      </p>
+
+      <p>
+        Miguel joins us from Facebook’s office in the UK to lead the keynote on
+        “The evolution of ReactJS and single page
+        applications - the past, the present, the future.”
+      </p>
+    `,
     image: "https://avatars1.githubusercontent.com/u/829269?s=460&v=4",
     githubUrl: "https://github.com/mjesun",
-    twitterUrl: "https://twitter.com/mjesun"
+    twitterUrl: "https://twitter.com/mjesun",
   },
   {
     name: "Christian Nwamba",
@@ -33,7 +108,7 @@ export const speakersList = [
     introText: "",
     image: "https://pbs.twimg.com/profile_images/725313461765427204/2Od92RM7_400x400.jpg",
     githubUrl: "https://github.com/christiannwamba",
-    twitterUrl: "https://twitter.com/codebeast"
+    twitterUrl: "https://twitter.com/codebeast",
   },
   {
     name: "Celestine Omin",
@@ -42,7 +117,7 @@ export const speakersList = [
     introText: "",
     image: "https://media.licdn.com/dms/image/C5603AQEYAraUiZHh7g/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=G8H4vPd26dkP1qJhyQuFO3WCgNfiDxVObfD9V3Luh_8",
     githubUrl: "https://github.com/cyberomin",
-    twitterUrl: "https://twitter.com/cyberomin"
+    twitterUrl: "https://twitter.com/cyberomin",
   },
   {
     name: "Chinedu Daniel",
@@ -51,6 +126,5 @@ export const speakersList = [
     introText: "",
     image: "https://media.licdn.com/dms/image/C5603AQEzGhypAHriqA/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=uv5ruHGlrj3FTkYnJDjFb5zpDydyg4-x8IZvK55qQZ0",
     githubUrl: "https://github.com/blueyedgeek",
-    twitterUrl: "https://twitter.com/austinswaggz"
   },
 ];
