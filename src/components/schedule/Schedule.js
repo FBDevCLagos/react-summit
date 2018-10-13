696a50635179bfa88712942e93a7a51062cc4d96
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-10-13 17:07:38 +0100

    Update the schedule section with relevant data

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -24,16 +24,44 @@ const Event = ({ type, time, title, anchor }) => {
   );
 };
 
+const TimelineOption = ({option, makeActiveTab, onClick}) => {
+  return (
+    <p
+      className={`timeline ${makeActiveTab(option)}`}
+      onClick={onClick}
+      data-tab-option={option}
+    >
+      {option}
+    </p>
+  );
+};
+
+const TimelineTab = ({makeActiveTab, onClick}) => {
+  return (
+    <div className="timelines">
+      <TimelineOption
+        option='beginner'
+        makeActiveTab={makeActiveTab}
+        onClick={onClick}
+      />
+
+      <TimelineOption
+        option='advanced'
+        makeActiveTab={makeActiveTab}
+        onClick={onClick}
+      />
+    </div>
+  );
+};
+
 export default class Schedule extends Component {
   state = {
-    activeTab: "primary"
+    activeTab: 'beginner'
   }
 
-  makeActiveTab(compareStr) {
-    const { activeTab } = this.state;
-
-    return activeTab === compareStr ? 'active' : '';
-  }
+  makeActiveTab = compareStr => (
+    this.state.activeTab === compareStr ? 'active' : ''
+  )
 
   listEvents(events) {
     return events.map(({ type, time, title, anchor }) => (
@@ -47,55 +75,45 @@ export default class Schedule extends Component {
     ));
   }
 
+  handleClick = (e) => {
+    this.setState({
+      activeTab: e.target.dataset.tabOption
+    });
+  }
+
   render () {
     return (
       <div id="schedule" className="side-panel__container schedule">
         <h2 className="side-panel__header-title">
-          <i className="fa fa-calendar-alt"></i>
           Schedule
         </h2>
 
         <div className="side-panel__content">
           <p>
-            React Summit will take place on the 19th and 20th of October 2018.
-            The details are being worked out and this section will be updated
-            with relevant information once it is available.
+            React Summit is scheduled for the 19th of October 2018
           </p>
+
+          <div className="events active">
+            { this.listEvents(schedule.events.shared) }
+          </div>
+
+          <h2>
+            Presentations && Workshops
+          </h2>
+
+          <TimelineTab
+            makeActiveTab={this.makeActiveTab}
+            onClick={this.handleClick}
+          />
+
+          <div className={`events ${this.makeActiveTab('beginner')}`}>
+            { this.listEvents( schedule.events.beginner ) }
+          </div>
+
+          <div className={`events ${this.makeActiveTab('advanced')}`}>
+            { this.listEvents( schedule.events.advanced ) }
+          </div>
         </div>
-
-        { false && <div className="side-panel__content">
-          <div className="timelines">
-            <p
-              className={`timeline ${this.makeActiveTab('primary')}`}
-              onClick={() => {
-                this.setState({
-                  activeTab: 'primary'
-                })
-              }}
-            >
-              Fri, Oct 19
-            </p>
-
-            <p
-              className={`timeline ${this.makeActiveTab('secondary')}`}
-              onClick={() => {
-                this.setState({
-                  activeTab: 'secondary'
-                })
-              }}
-            >
-              Sat, Oct 20
-            </p>
-          </div>
-
-          <div className={`events ${this.makeActiveTab('primary')}`}>
-            { this.listEvents( schedule.firstDayEvents ) }
-          </div>
-
-          <div className={`events ${this.makeActiveTab('secondary')}`}>
-            { this.listEvents( schedule.secondDayEvents ) }
-          </div>
-        </div> }
       </div>
     );
   }
