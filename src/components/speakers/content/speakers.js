dc2411d82ba160952940a3c69acd047ba5a42be4
Innocent <innocent.amadi@andela.com>
2018-10-15 20:48:41 +0100

    add speakers bios

--- src/components/speakers/content/speakers.js
+++ src/components/speakers/content/speakers.js
@@ -103,28 +103,122 @@ export const speakersList = [
   },
   {
     name: "Christian Nwamba",
+<<<<<<< HEAD
     title: "Content Creator / Technical Reviewer",
     company: "Pusher",
     introText: `<p>Developer Advocate for the Next Billion Developers.</p>`,
+=======
+    title: "Developer and Developer Advocate, Author",
+    company: "Cloudinary & Pusher",
+    introText: `
+      <p>
+        Chris is a Developer Advocate for the Next Billion Developers. 
+        He is a JavaScript preacher and author of http://honext.io.
+      </p>
+      <p>
+        Christian will be taking us through a hands-on workshop with live examples on how to measure and optimize performance in React apps.
+      </p>
+    `,
+>>>>>>> add speakers bios
     image: "https://pbs.twimg.com/profile_images/725313461765427204/2Od92RM7_400x400.jpg",
     githubUrl: "https://github.com/christiannwamba",
     twitterUrl: "https://twitter.com/codebeast",
   },
   {
-    name: "Celestine Omin",
-    title: "Senior Technical Consultant",
-    company: "Andela",
-    introText: "",
-    image: "https://media.licdn.com/dms/image/C5603AQEYAraUiZHh7g/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=G8H4vPd26dkP1qJhyQuFO3WCgNfiDxVObfD9V3Luh_8",
-    githubUrl: "https://github.com/cyberomin",
-    twitterUrl: "https://twitter.com/cyberomin",
-  },
-  {
-    name: "Chinedu Daniel",
+    name: "Ugonna Thelma Ofoegbu",
     title: "Software Developer",
     company: "Andela",
-    introText: "",
-    image: "https://media.licdn.com/dms/image/C5603AQEzGhypAHriqA/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=uv5ruHGlrj3FTkYnJDjFb5zpDydyg4-x8IZvK55qQZ0",
-    githubUrl: "https://github.com/blueyedgeek",
+    introText: `
+      <p>
+        Ugonna works at Zego.io as a front-end developer. When she is not coding, she illustrates for children's storybooks and design user interfaces.
+      </p>
+      <p>
+       Ugonna's talk will cover why we test components, what exactly we test for, how to test for these things, down to mocking methods and events. 
+      </p>
+    `,
+    image: "https://farm2.staticflickr.com/1903/45344502861_ac80a67ca9_k.jpg",
+    githubUrl: "https://github.com/ugonnathelma",
+    twitterUrl: "https://twitter.com/ugonna_t",
+  },
+  {
+    name: "Robert John Thas",
+    title: "Data Engineer, ML Engineer",
+    company: "EnterFive",
+    introText: `
+      <p>
+        Robert John Thas is Data Engineer and Google Developer Expert certified Machine Learning Engineer. 
+      </p>
+      <p>
+        Robert's workshop will guide JavaScript developers through consuming pre-trained ML models on the Google Cloud Platform. The solution will take an image, read the text in the image, and translate it into Hausa, Igbo, and Yoruba. This will serve to show participants how to implement localization in their React and React-Native Applications
+      </p>
+    `,
+    image: "https://farm2.staticflickr.com/1976/30405249757_a10b07b2bd.jpg",
+    githubUrl: "https://github.com/robert_thas",
+  },
+  {
+    name: "Kesiena Akpobome",
+    title: "Technical Team Lead",
+    company: "Andela",
+    introText: `
+      <p>
+        Kesiena is a Software Artist (self proclaimed), Innovation and scalable future enthusiast.
+      </p>
+      <p>
+        Kesiena's talk walks us practically through the whys and hows of using React with Typescript.
+      </p>
+    `,
+    image: "https://farm2.staticflickr.com/1957/43530226800_9c3fefd76c.jpg",
+    githubUrl: "https://github.com/robert_thas",
+  },
+  {
+    name: "Adewale Abati",
+    title: "Software Developer",
+    company: "Ingressive",
+    introText: `
+      <p>
+        Abati Adewale, also known as Ace is a Web Developer and Advocate at Ingressive. He is passionate about open source, the web and basketball.
+      </p>
+      <p>
+        Ace also runs a YouTube channel in his name documenting his journey through tech and development and other aspects of the lifestyle of a techie from Nigeria. (https://www.youtube.com/acekyd).
+      </p>
+      <p>
+        This talk will go over why you should make create-react-app your best friend and go to boilerplate for building React apps. Can and should you use it in production? and many more tips surrounding the boilerplate.      
+      </p>
+    `,
+    image: "https://farm2.staticflickr.com/1939/44622238534_186f2892f1.jpg",
+    githubUrl: "https://github.com/robert_thas",
+  },
+  {
+    name: "Timi Aiyemo",
+    title: "Senior Software Consultant",
+    company: "Andela",
+    introText: `
+      <p>
+        Timi loves application development and opportunities to share ideas with others. He recently facilitated <a target="_blank" href="eventplog.com/c/devclagos/e/rw4">4 weeks of React workshops</a> and found himself drawn to the struggles of the beginnings.
+      </p>
+      <p>
+        Timi's talk will capture the 10 top things React newbies should always remember when starting the journey.
+      </p>
+    `,
+    image: "https://farm2.staticflickr.com/1977/44432146395_057eb4819e.jpg",
+    twitterUrl: "https://twitter.com/cozzbie",
+  },
+  {
+    name: "Fiyinfoluwa Adebayo",
+    title: "Senior Software Engineer",
+    company: "Andela",
+    introText: `
+      <p>
+        Fiyin is a Senior Software Engineer at Andela and Happy Money, Product Designer at Kompilab. He currently leads a team to extend the functionality of a self-service portal and integrate their microservices architecture with a Salesforce solution to enable servicing of more applications per customer.
+      </p>
+      <p>
+        Fiyin is an advocate for using the right tools to build products that users understand and love. UI/UX and cars are favourite topics he loves to discuss over cake and coffee.
+      <p>
+        Fiyin would be taking us through an introduction to understanding Electron and how we can use React to build cross platform desktop apps while working through an example.
+      </p>
+    `,
+    image: "https://farm2.staticflickr.com/1954/30406297357_529abce77d.jpg",
+    githubUrl: "https://github.com/andela-fadebayo",
+    twitterUrl: "https://twitter.com/ceemion"
   },
 ];
